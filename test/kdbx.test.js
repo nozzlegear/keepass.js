ce94e46d0813e1833b97f317a2bbd97ae626904c
Ulich, Timo <timo@ulich.net>
2015-09-06 15:14:45 +0200

    test file attachments in kdbx databases to increate test coverage

--- test/kdbx.test.js
+++ test/kdbx.test.js
@@ -36,18 +36,33 @@ describe("kdbx", () => {
     
     it("should decrypt a kdb file", (done) => {
         fetchArrayBuffer('base/test/data/database_with_password.kdb.dat').then((fileContents) => {
-            decryptDatabaseAndVerify(done, fileContents, "test");
+            var db = new Keepass.Database();
+
+            return db.getPasswords(fileContents, "test")
+                .then((entries) => {
+                    expect(entries.length).toBe(1);
+
+                    var entry = entries[0];
+                    expect(entry.keys).toEqual([ 'title', 'url', 'userName', 'notes' ]);
+                    expect(entry.groupName).toBe("test group");
+                    expect(entry.title).toBe("test_entry");
+                    expect(entry.userName).toBe("test_username");
+                    expect(db.decryptProtectedData(entry.protectedData.password, db.streamKey)).toBe("test_password");
+
+                    done();
+                }, done.fail);
         }, done.fail);
     });
 
     function decryptDatabaseAndVerify(done, fileContents, password, keyFile) {
         var db = new Keepass.Database();
 
-        db.getPasswords(fileContents, password, keyFile)
+        return db.getPasswords(fileContents, password, keyFile)
             .then((entries) => {
                 expect(entries.length).toBe(1);
 
                 var entry = entries[0];
+                expect(entry.keys).toEqual(['notes', 'title', 'url', 'userName', 'binaryFiles']);
                 expect(entry.groupName).toBe("test group");
                 expect(entry.title).toBe("test_entry");
                 expect(entry.userName).toBe("test_username");
