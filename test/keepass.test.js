3c324861b37ce20fb1acfe18070ad5cead898fe1
Ulich, Timo <timo@ulich.net>
2015-09-10 20:53:12 +0200

    merge kdb.test.js and kdbx.test.js into one file as they actually test keepass.js from end to end

--- test/keepass.test.js
+++ test/keepass.test.js
@@ -41,36 +41,52 @@ describe("keepass.js", () => {
     });
 
     it("should throw an error when reading a non database file", (done) => {
+        var db = new Keepass.Database();
         fetchArrayBuffer('base/test/data/key_file_random.dat').then((fileContents) => {
-            var db = new Keepass.Database();
+            return db.getPasswords(fileContents, "test");
+        }).catch((msg) => {
+            expect(msg).toBe('Invalid KeePass file - file signature is not correct. (ef5a43a6:a876235a)');
+            done();
+        });
+    });
 
-            db.getPasswords(fileContents, "test").catch((msg) => {
-                expect(msg).toBe('Invalid KeePass file - file signature is not correct. (ef5a43a6:a876235a)');
-                done();
-            });
+    it("should decrypt a kdb file", (done) => {
+        var db = new Keepass.Database();
+        fetchArrayBuffer('base/test/data/database_with_password.kdb.dat').then((fileContents) => {
+            return db.getPasswords(fileContents, "test");
+        }).then((entries) => {
+            expect(entries.length).toBe(1);
+
+            var entry = entries[0];
+            expect(entry.keys).toEqual([ 'title', 'url', 'userName', 'notes' ]);
+            expect(entry.groupName).toBe("test group");
+            expect(entry.title).toBe("test_entry");
+            expect(entry.userName).toBe("test_username");
+            expect(db.decryptProtectedData(entry.protectedData.password)).toBe("test_password");
+
+            done();
         }, done.fail);
     });
 
     function decryptDatabaseAndVerify(done, fileContents, password, keyFile) {
         var db = new Keepass.Database();
 
-        return db.getPasswords(fileContents, password, keyFile)
-            .then((entries) => {
-                expect(entries.length).toBe(1);
+        return db.getPasswords(fileContents, password, keyFile).then((entries) => {
+            expect(entries.length).toBe(1);
 
-                var entry = entries[0];
-                expect(entry.keys).toEqual(['tags', 'additionalFieldWithNoValue', 'additionalFieldWithValue', 'notes', 'title', 'url', 'userName', 'binaryFiles']);
-                expect(entry.groupName).toBe("test group");
-                expect(entry.title).toBe("test_entry");
-                expect(entry.userName).toBe("test_username");
-                expect(db.decryptProtectedData(entry.protectedData.password)).toBe("test_password");
-                expect(entry.additionalFieldWithNoValue).toBe("");
-                expect(entry.additionalFieldWithValue).toBe("some value with\nnewlines");
-                expect(db.decryptProtectedData(entry.protectedData.additionalEncryptedFieldNoValue)).toBe("");
-                expect(db.decryptProtectedData(entry.protectedData.additionalEncryptedFieldWithValue)).toBe("some encrypted value with\r\nnewlines");
-                expect(entry.tags).toBe('keepass.js test');
+            var entry = entries[0];
+            expect(entry.keys).toEqual(['tags', 'additionalFieldWithNoValue', 'additionalFieldWithValue', 'notes', 'title', 'url', 'userName', 'binaryFiles']);
+            expect(entry.groupName).toBe("test group");
+            expect(entry.title).toBe("test_entry");
+            expect(entry.userName).toBe("test_username");
+            expect(db.decryptProtectedData(entry.protectedData.password)).toBe("test_password");
+            expect(entry.additionalFieldWithNoValue).toBe("");
+            expect(entry.additionalFieldWithValue).toBe("some value with\nnewlines");
+            expect(db.decryptProtectedData(entry.protectedData.additionalEncryptedFieldNoValue)).toBe("");
+            expect(db.decryptProtectedData(entry.protectedData.additionalEncryptedFieldWithValue)).toBe("some encrypted value with\r\nnewlines");
+            expect(entry.tags).toBe('keepass.js test');
 
-                done();
-            }, done.fail);
+            done();
+        }, done.fail);
     }
 });
--- test/libs/test-utils.js
+++ test/libs/test-utils.js
@@ -1,6 +1,5 @@
 export function fetchArrayBuffer(path) {
-    return fetch(path)
-        .then(function (response) {
-            return response.arrayBuffer(); 
-        });
+    return fetch(path).then(function (response) {
+        return response.arrayBuffer(); 
+    });
 };
